645d59d09ef7f70a6771fc4cce16fbcad6d0c291
Thomas <54805624+thomas-samoht@users.noreply.github.com>
2026-09-17 09:51:03 +0200

    Use poetry instead of makefile (#11)
    * Use poetry instead of makefile
    
    * Added ruff format --diff
    
    * Fix formatting

--- app/events.py
+++ app/events.py
@@ -4,8 +4,18 @@ _Base = DefaultEventCatalogue
 
 
 class Log(_Base):
-    SYS_APP_STARTED = _Base.SYS_APP_STARTED.with_id("270401")  # PRS-SYS-001 (APP stream only per spec)
-    SYS_APP_STOPPED = _Base.SYS_APP_STOPPED.with_id("270402")  # PRS-SYS-002 (controlled shutdown)
-    SYS_APP_CRASHED = _Base.SYS_APP_CRASHED.with_id("270402")  # PRS-SYS-002 (uncontrolled shutdown)
-    SYS_UNHANDLED_EXCEPTION = _Base.SYS_UNHANDLED_EXCEPTION.with_id("270404")  # PRS-SYS-004
-    SYS_MISSING_CORRELATION_ID = _Base.SYS_MISSING_CORRELATION_ID.with_id("270407")  # PRS-SYS-007
+    SYS_APP_STARTED = _Base.SYS_APP_STARTED.with_id(
+        "270401"
+    )  # PRS-SYS-001 (APP stream only per spec)
+    SYS_APP_STOPPED = _Base.SYS_APP_STOPPED.with_id(
+        "270402"
+    )  # PRS-SYS-002 (controlled shutdown)
+    SYS_APP_CRASHED = _Base.SYS_APP_CRASHED.with_id(
+        "270402"
+    )  # PRS-SYS-002 (uncontrolled shutdown)
+    SYS_UNHANDLED_EXCEPTION = _Base.SYS_UNHANDLED_EXCEPTION.with_id(
+        "270404"
+    )  # PRS-SYS-004
+    SYS_MISSING_CORRELATION_ID = _Base.SYS_MISSING_CORRELATION_ID.with_id(
+        "270407"
+    )  # PRS-SYS-007
